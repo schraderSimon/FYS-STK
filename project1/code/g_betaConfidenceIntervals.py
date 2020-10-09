c32496691a43a3538102c1d74983cd1c1fefaac3
Adrian Kleven <adriankleven@hotmail.com>
2020-10-09 22:43:32 +0200

    csv

--- project1/code/g_betaConfidenceIntervals.py
+++ project1/code/g_betaConfidenceIntervals.py
@@ -30,6 +30,8 @@ Z_table = 1.96
 lambda_val_ridge = [0,0.01,1]
 
 std_dev_ridge = np.zeros((maxdeg,len(lambda_val_ridge)))
+max_beta = np.zeros((maxdeg,len(lambda_val_ridge)))
+min_beta = np.zeros((maxdeg,len(lambda_val_ridge)))
 counter = 0
 for deg in range(1,maxdeg+1):
     X=DesignMatrix_deg2(x,y,deg) #create design matrix
@@ -50,6 +52,8 @@ for deg in range(1,maxdeg+1):
         z_diff = z_test_scaled-X_test_scaled @ beta_ridge
         sigma_squared_estimate = 1/(len(z_test_scaled)-p-1)*(z_diff @ z_diff)
         std_dev_ridge[counter,i] = round(np.sqrt(np.amax(beta_var_ridge*sigma_squared_estimate)),2)
+        max_beta[counter,i] = np.round(np.amax(beta_ridge),2)
+        min_beta[counter,i] = np.round(np.amin(beta_ridge),2)
     counter +=1
 
 print('Random seed: '+'sum([ord(c) for c in "CORONA"]'+', datapoints = {0} ,Z_table = {2}, deg.max. = {1}, lambda_vals={3}'.format(datapoints,maxdeg,Z_table,lambda_val_ridge))
@@ -60,6 +64,7 @@ print('Max Std.dev. Ridge')
 print(std_dev_ridge)
 
 """ OUTPUT CSV """
-dict = {'polynomial degree': [i for i in range(1,maxdeg+1)], 'OLS':  np.round(Z_table*std_dev_ridge[:,0]), 'Ridge (lambda = {0})'.format(lambda_val_ridge[1]): np.round(Z_table*std_dev_ridge[:,1]),'Ridge (lambda = {0})'.format(lambda_val_ridge[2]): np.round(Z_table*std_dev_ridge[:,2])}
+dict = {'polynomial degree': [i for i in range(1,maxdeg+1)], 'OLS Max std. dev.':  np.round(Z_table*std_dev_ridge[:,0],2), 'OLS_beta_max':  max_beta[:,0], 'OLS_beta_min':  min_beta[:,0],
+'Ridge Max std. dev. (lambda = {0})'.format(lambda_val_ridge[1]): np.round(Z_table*std_dev_ridge[:,1],2), 'Ridge_beta_max(lambda = {0})'.format(lambda_val_ridge[1]):  max_beta[:,1],'Ridge_beta_max(lambda = {0})'.format(lambda_val_ridge[1]):  min_beta[:,1],'Ridge Max std. dev. (lambda = {0})'.format(lambda_val_ridge[2]): np.round(Z_table*std_dev_ridge[:,2],2), 'Ridge_beta_max(lambda = {0})'.format(lambda_val_ridge[2]):  max_beta[:,2], 'Ridge_beta_max(lambda = {0})'.format(lambda_val_ridge[2]):  min_beta[:,2]}
 df = pd.DataFrame(dict)
 df.to_csv('C:/Users/adria/Documents/Studier/FYS-STK4155/FYS-STK/project1/csvData/g_confidence_intervals_maxdeg({0}).csv'.format(maxdeg))
