325006199c86983ae0573459bddee35e48f59959
Adrian Kleven <adriankleven@hotmail.com>
2020-10-09 22:12:48 +0200

    small report changes, typo in plotgeographicdata.py, added confidence intervals for terrain

---
+++ project1/code/g_betaConfidenceIntervals.py
@@ -0,0 +1,65 @@
+import numpy as np
+from sklearn.preprocessing import StandardScaler, Normalizer, RobustScaler,MinMaxScaler
+from small_function_library import *
+import matplotlib.pyplot as plt
+from imageio import imread
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import sys
+
+"""make the data"""
+terrain = imread("../data/Korea.tif") #Open image file
+np.random.seed(sum([ord(c) for c in "CORONA"]))
+datapoints= 50000 #number of sample points
+
+
+x=np.random.randint(len(terrain),size=datapoints) #random integers
+y=np.random.randint(len(terrain[1]),size=datapoints) #random integers for y
+xy_array=np.column_stack((x,y))
+z=[]
+for xv,yv in xy_array:
+    z.append(terrain[xv,yv])
+z=np.array(z) #data to be fitted
+
+"""Set initial parameters"""
+
+nr_lambdas_ridge = 3
+
+maxdeg=5
+Z_table = 1.96
+lambda_val_ridge = [0,0.01,1]
+
+std_dev_ridge = np.zeros((maxdeg,len(lambda_val_ridge)))
+counter = 0
+for deg in range(1,maxdeg+1):
+    X=DesignMatrix_deg2(x,y,deg) #create design matrix
+    X_train, X_test, z_train, z_test = train_test_split(X,z, test_size=0.25) #split data
+    z_train_scaled=z_train-np.mean(z_train) #subsract average
+    z_test_scaled=z_test-np.mean(z_train)#subsract average
+    scaler=StandardScaler()
+    scaler.fit(X_train)
+    X_train_scaled=scaler.transform(X_train)
+    X_test_scaled=scaler.transform(X_test)
+
+    p = len(X[0,:])+1
+    print('polydegree={0}'.format(int(deg)))
+
+    for i in range(len(lambda_val_ridge)):
+        beta_ridge, beta_var_ridge = RidgeRegression(X_train_scaled,z_train_scaled,lambda_val_ridge[i])
+
+        z_diff = z_test_scaled-X_test_scaled @ beta_ridge
+        sigma_squared_estimate = 1/(len(z_test_scaled)-p-1)*(z_diff @ z_diff)
+        std_dev_ridge[counter,i] = round(np.sqrt(np.amax(beta_var_ridge*sigma_squared_estimate)),2)
+    counter +=1
+
+print('Random seed: '+'sum([ord(c) for c in "CORONA"]'+', datapoints = {0} ,Z_table = {2}, deg.max. = {1}, lambda_vals={3}'.format(datapoints,maxdeg,Z_table,lambda_val_ridge))
+
+
+
+print('Max Std.dev. Ridge')
+print(std_dev_ridge)
+
+""" OUTPUT CSV """
+dict = {'polynomial degree': [i for i in range(1,maxdeg+1)], 'OLS':  Z_table*std_dev_ridge[:,0], 'Ridge (lambda = {0})'.format(lambda_val_ridge[1]): Z_table*std_dev_ridge[:,1],'Ridge (lambda = {0})'.format(lambda_val_ridge[1]): Z_table*std_dev_ridge[:,2]}
+df = pd.DataFrame(dict)
+df.to_csv('C:/Users/adria/Documents/Studier/FYS-STK4155/FYS-STK/project1/csvData/g_confidence_intervals_maxdeg({0}).csv'.format(maxdeg))
